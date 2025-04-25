254521675f50f9c567ae9e2fe31f1dc07afd9f40
Imam Fajri <imamfajri.ze@gmail.com>
2025-04-26 00:13:37 +0700

    Added fiture front-end authentication

---
+++ authentication/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from authentication.views import *
+
+app_name = "authentication"
+
+urlpatterns = [
+    path("login/", login_view, name="login"),
+    path("register/dokter", register_dokter_view, name="register_dokter"),
+    path("register/pengunjung", register_pengunjung_view, name="register_pengunjung"),
+    path("register/staff", register_staf_view, name="register_staf"),
+]
