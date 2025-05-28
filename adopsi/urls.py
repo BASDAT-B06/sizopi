da21cb7e764c7fbde6b35d7ac097ee8a70a82cd3
naphiale <nafia.levana19@gmail.com>
2025-05-28 18:35:54 +0700

    feat: edit urls and views to connect with service

--- adopsi/urls.py
+++ adopsi/urls.py
@@ -7,10 +7,20 @@ urlpatterns = [
     # URLs untuk Staf Administrasi
     path('kelola-adopsi/', views.adoption_list_admin, name='admin_list'),
     path('kelola-adopter/', views.adopter_list, name='adopter_list'),
-    path('adopter/<int:adopter_id>/history/', views.adopter_history, name='adopter_history'),
+    path('adopter/<str:adopter_id>/history/', views.adopter_history, name='adopter_history'),
     
     # URLs untuk Pengunjung (Adopter)
-    path('user/', views.adoption_list_user, name='user_list'),
-    path('user/certificate/<int:animal_id>/', views.adoption_certificate, name='certificate'),
-    path('user/report/<int:animal_id>/', views.animal_report, name='animal_report'),
+    path('', views.adoption_list_user, name='user_list'),
+    path('certificate/<str:animal_id>/', views.adoption_certificate, name='certificate'),
+    path('report/<str:animal_id>/', views.animal_report, name='animal_report'),
+    
+    # AJAX endpoints
+    path('verify-adopter/', views.verify_adopter_account, name='verify_adopter'),
+    path('create-adoption-individu/', views.create_adoption_individu, name='create_adoption_individu'),
+    path('create-adoption-organisasi/', views.create_adoption_organisasi, name='create_adoption_organisasi'),
+    path('update-payment-status/', views.update_payment_status, name='update_payment_status'),
+    path('stop-adoption/', views.stop_adoption, name='stop_adoption'),
+    path('delete-adopter/', views.delete_adopter, name='delete_adopter'),
+    path('delete-adoption-history/', views.delete_adoption_history, name='delete_adoption_history'),
+    path('extend-adoption/', views.extend_adoption, name='extend_adoption'),
 ]
