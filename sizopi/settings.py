45527b5ab25a5a96a83612284c9f78d93fdcbf84
freiazulaika <freiazulaika27@gmail.com>
2025-05-29 14:33:59 +0700

    add static

--- sizopi/settings.py
+++ sizopi/settings.py
@@ -158,10 +158,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
-else:
-    STATIC_ROOT = BASE_DIR / 'static'
+
+# Ini direktori output hasil collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Ini direktori asal file statis milikmu (sudah benar)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
