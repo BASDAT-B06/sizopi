e73ef0c06e9ddcd8255a1286d048822b31e17256
dhaniatiara <dhania.t.herdiani@gmail.com>
2025-05-29 14:14:34 +0700

    ha

--- sizopi/settings.py
+++ sizopi/settings.py
@@ -105,9 +105,12 @@ WSGI_APPLICATION = 'sizopi.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',  # Using SQLite
